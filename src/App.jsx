26652ab32a0824305f2561b1da4dd11a6db5cbe9
akashsingh_bhandari <asbhandari14@gmail.com>
2024-04-09 04:28:58 +0530

    Creating the Title of the Website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css"
+import Title from "./Components/Title";
+
+
+
+const App=()=>{
+  return (
+    <>
+      <Title/>
+    </>
+  )
+}
+
+export default App;
